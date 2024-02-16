d17e92d3a4b8d9209c9b4b0bb1efda6804206b57
Batyr Atayev <atayev2012@gmail.com>
2024-02-16 20:05:19 +0300

    Added multiplication and division. - minor fix with printValue

--- src/ComplexNumber.java
+++ src/ComplexNumber.java
@@ -6,7 +6,41 @@ public class ComplexNumber {
         this.imaginaryNumber = i;
     }
 
+    // Add complex number
+    public void addComplex(ComplexNumber a){
+        this.realNumber += a.realNumber;
+        this.imaginaryNumber += a.imaginaryNumber;
+    }
+
+    // Subtract complex number
+    public void subComplex(ComplexNumber a){
+        this.realNumber -= a.realNumber;
+        this.imaginaryNumber -= a.imaginaryNumber;
+    }
+
+    // Multiplication of complex numbers
+    public void multiplyComplex(ComplexNumber a){
+        int r = this.realNumber * a.realNumber - this.imaginaryNumber * a.imaginaryNumber;
+        int i = this.realNumber * a.imaginaryNumber + this.imaginaryNumber * a.realNumber;
+        this.realNumber = r;
+        this.imaginaryNumber = i;
+    }
+
+    // Division of complex numbers
+    public void divideComplex(ComplexNumber a){
+        int d = a.realNumber * a.realNumber + a.imaginaryNumber * a.imaginaryNumber;
+        int r = (this.realNumber * a.realNumber + this.imaginaryNumber * a.imaginaryNumber) / d;
+        int i = (this.imaginaryNumber * a.realNumber - this.realNumber * a.imaginaryNumber) / d;
+        this.realNumber = r;
+        this.imaginaryNumber = i;
+    }
+
+    // Output values
     public void printValue(){
-        System.out.println(this.realNumber + (this.imaginaryNumber > 0 ? "+":"") + this.imaginaryNumber + "i");
+        String text = "" + this.realNumber;
+        if(this.imaginaryNumber != 0){
+            text += (this.imaginaryNumber > 0 ? "+":"") + this.imaginaryNumber + "i";
+        }
+        System.out.println(text);
     }
 }
