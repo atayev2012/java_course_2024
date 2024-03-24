8f4acbe60045c23bcbd1340c92cb04eef2c03e56
Batyr Atayev <atayev2012@gmail.com>
2024-03-24 20:30:19 +0300

    initialization of Main.java

---
+++ src/lab_02/Main.java
@@ -0,0 +1,10 @@
+package lab_02;
+
+import java.io.IOException;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        LetterCounter data = new LetterCounter();
+        data.mainMenu();
+    }
+}
